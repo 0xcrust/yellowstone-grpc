05c5a2bff9c324b29f042ef3431b2213d4d448e9
Ademola <83315082+0xcrust@users.noreply.github.com>
2024-07-27 16:46:20 +0100

    Support overriding protoc path

--- yellowstone-grpc-proto/build.rs
+++ yellowstone-grpc-proto/build.rs
@@ -1,5 +1,7 @@
 fn main() -> anyhow::Result<()> {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
+    if std::env::var("PROTOC").is_err() {
+        std::env::set_var("PROTOC", protobuf_src::protoc());
+    }
     tonic_build::compile_protos("proto/yellowstone-log.proto")?;
     Ok(())
 }
